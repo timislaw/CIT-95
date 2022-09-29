8941baa2945f0e0235b01a220b01105f0cb1abfc
Tim Ross <personal.timross@gmail.com>
2022-09-29 14:21:53 -0700

    need to iron out the incorrect inputs

--- code-challenge-2/challenge-2-my-solution.py
+++ code-challenge-2/challenge-2-my-solution.py
@@ -1,41 +1,47 @@
 #smallest biggest integer game
 #tackle advanced todos
 from smtpd import DebuggingServer
-import string
 
 usr_input = []
 usr_inc_input = []
 
-usr_inc_input.append('string')
-usr_inc_input.append('string')
-usr_inc_input.append('string')
+# new_variable = 'string'
 
+# if new_variable == str:
+#     print('its a string')
+# elif (new_variable >= 0):
+#     print('number')
+# else:
+#     print('something else')
 
-def value_pusher(item):
+#isinstance is best option to see if the value is a certain datatype
+num = None
+stringest = None
+largest = 0
+smallest = 0
+clean = True
+for i in range(20):
+    new_input = input(
+        "Put in a number up to 20 times or if done, type 'done'\n")
+    for j in range(10):
+        if str(j) in new_input:
+            num = int(new_input)
+            usr_input.append(num)
+            break
 
-    if (item.contains() and item.lower() != 'done'):
-        usr_inc_input.append(item)
-        print(usr_inc_input)
-    elif (item >= 0 or item >= 0.0):
-        usr_input.append(item)
-        print(usr_input)
+    if (clean):
+        if (num > largest):
+            largest = num
+        elif (num < smallest):
+            smallest = num
+
+    if (new_input.lower() == 'done' or i == 20):
+        break
     else:
-        print('done')
+        stringest = new_input
+        usr_inc_input.append(stringest)
+        clean = False
 
-
-# value_pusher('string')
-
-# value_pusher(50)
-
-# value_pusher(5.66)
-
-new_variable = 'string'
-
-if new_variable == string:
-    print('its a string')
-elif (int(new_variable) >= 0):
-    print('number')
-else:
-    print('something else')
-
-#test
+print(usr_input)
+print(usr_inc_input)
+print(largest, smallest)
